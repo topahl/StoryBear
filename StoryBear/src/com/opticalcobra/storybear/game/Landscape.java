271848d2d6e05be4e63eb15640dd9244141a8e57
Miriam <Miriam@MI-I15z>
2014-04-03 19:36:48 +0200

    neue Klassen für Character (Hexe, kleines Mädchen, ...), Collectables und Landscape
    Change-Id: Iad2bd7f4b11d2aac5888c309ff81fea2d23aad1c
    Signed-off-by: Miriam <Miriam@MI-I15z>

---
+++ StoryBear/src/com/opticalcobra/storybear/game/Landscape.java
@@ -0,0 +1,19 @@
+package com.opticalcobra.storybear.game;
+
+import com.opticalcobra.storybear.main.ILevelAppearance;
+
+public class Landscape implements ILevelAppearance {
+
+	@Override
+	public int getBlock() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setBlock(int block) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
