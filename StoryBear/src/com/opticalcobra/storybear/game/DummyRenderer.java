e55615c07dad0a913b5efe94adebe1099a672b7e
Miriam <Miriam@MI-I15z>
2014-04-10 19:40:31 +0200

    Text unter Level schreiben (noch kleiner Bug drin)
    Change-Id: I79a32e214101b68348133218d5093e45d546da89
    Signed-off-by: Miriam <Miriam@MI-I15z>

--- StoryBear/src/com/opticalcobra/storybear/game/DummyRenderer.java
+++ StoryBear/src/com/opticalcobra/storybear/game/DummyRenderer.java
@@ -13,6 +13,8 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 
 import com.opticalcobra.storybear.debug.DebugSettings;
+import com.opticalcobra.storybear.editor.StoryInfo;
+import com.opticalcobra.storybear.main.ILevelAppearance;
 import com.opticalcobra.storybear.res.FontCache;
 import com.opticalcobra.storybear.res.Imagelib;
 import com.opticalcobra.storybear.res.Ressources;
@@ -23,6 +25,8 @@ public class DummyRenderer extends Renderer implements IRenderer{
 	private int lastTile = 0;
 	private int panelnum = 0;
 	private ArrayList<int[]>dependencies;
+	private StoryInfo storyInfo;
+	private int elementPointer = 0;
 	
 	public DummyRenderer(){
 		dependencies = new ArrayList<int[]>();
@@ -55,6 +59,28 @@ public class DummyRenderer extends Renderer implements IRenderer{
 		dependencies.add(new int[]{27,28});
 		dependencies.add(new int[]{28});
 		dependencies.add(new int[]{0});
+		
+		
+		
+		
+		//Dummy Code	--> TODO: delete, dafür storyInfo übergeben lassen
+		this.storyInfo = new StoryInfo();
+		ArrayList<ILevelAppearance> elements = new ArrayList();
+		elements.add(new Word("eins",1));
+		elements.add(new Word("zwei",2));
+		elements.add(new Word("fünf",5));
+		elements.add(new Word("neun",9));
+		elements.add(new Word("zehn",10));
+		elements.add(new Word("vierzehn",14));
+		elements.add(new Word("siebzehn",17));
+		elements.add(new Word("zwanzig",20));
+		elements.add(new Word("dreiundzwanzig",23));
+		elements.add(new Word("fünfundzwanzig",25));
+		elements.add(new Word("hallo",33));
+		elements.add(new Word("hallo",36));
+		elements.add(new Word("hallo",39));
+		elements.add(new Word("hallo",42));
+		this.storyInfo.setElements(elements);
 	}
 	
 	private BufferedImage getNextMapElement(){
@@ -67,6 +93,9 @@ public class DummyRenderer extends Renderer implements IRenderer{
 	
 	@Override
 	public void getNextViewPart(JLabel pane) {
+		ArrayList<ILevelAppearance> elements = this.storyInfo.getElements();
+		
+		
 		panelnum++;
 		BufferedImage image = new BufferedImage(Ressources.WINDOW.width, Ressources.WINDOW.height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = (Graphics2D) image.getGraphics();
@@ -76,12 +105,14 @@ public class DummyRenderer extends Renderer implements IRenderer{
 				renderText(g,25, lastTile+"", (i*Ressources.RASTERSIZE)+20,100);
 			if(DebugSettings.vg1panelborder)
 				g.drawRect(i*Ressources.RASTERSIZE, 0, Ressources.RASTERSIZE, Ressources.WINDOW.height);
+	
+			if((elementPointer < this.storyInfo.getElements().size()) && (((Word)elements.get(elementPointer)).getBlock() == (i + (panelnum-1)*17))){
+				((Word)elements.get(elementPointer)).render(g);
+				elementPointer++;
+			}
 		}
 		if(DebugSettings.vg1panelnum)
 			renderText(g,50, panelnum+"", 20, 40);
-		
-		
-		
 		
 		pane.setIcon(new ImageIcon(image));
 	}
