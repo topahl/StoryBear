2ddf913602dcb79bad04097570ffdb941cf558c3
Tobias <pahlings@gmail.com>
2014-04-16 19:48:09 +0200

    Ringbuffer hinzugefügt
    Change-Id: Id46b34033914ab4a1769149f3962e2448fa8181a
    Signed-off-by: Tobias <pahlings@gmail.com>

---
+++ StoryBear/src/com/opticalcobra/storybear/main/Ringbuffer.java
@@ -0,0 +1,67 @@
+package com.opticalcobra.storybear.main;
+
+public class Ringbuffer<T> {
+	private Element read ;
+	private Element write;
+	
+	public Ringbuffer(int size){
+		Element first = new Element(null);
+		for(int i=1; i<size;i++){
+			first.add();
+		}
+		read = first;
+		write = first;
+		
+	}
+	
+	public T read(){
+		
+		while(read == write)
+			Thread.yield();
+		T result = read.getValue();
+		read = read.getNext();
+		return result;
+	}
+	
+	public  void write(T value){
+		while(write.getNext() == read){
+			Thread.yield();
+		}
+		write.setValue(value);
+		write = write.getNext();
+	}
+	
+	private class Element{
+		private Element next;
+		private T value;
+		
+		public Element(T value){
+			this.next = this;
+			this.value = value;
+		}
+
+		public Element getNext() {
+			return next;
+		}
+
+		public void setNext(Element next) {
+			this.next = next;
+		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public void setValue(T value) {
+			this.value = value;
+		}
+		public void add(){
+			Element neu = new Element(null);
+			neu.setNext(this.getNext());
+			this.setNext(neu);
+			
+		}
+		
+
+	}
+}
