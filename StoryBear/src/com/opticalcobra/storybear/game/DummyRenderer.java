3fa4750c7c1001e69ee1bd1abcca82e865d6c46c
Martika <martikaschwan@yahoo.de>
2014-04-18 15:44:49 +0200

    bugfix beim Rendern
    Change-Id: I9a5ef098722ecd936f2cd21f69ab0e96a577499c
    Signed-off-by: Martika <martikaschwan@yahoo.de>

--- StoryBear/src/com/opticalcobra/storybear/game/DummyRenderer.java
+++ StoryBear/src/com/opticalcobra/storybear/game/DummyRenderer.java
@@ -54,7 +54,7 @@ public class DummyRenderer extends Renderer implements IRenderer{
 		panelnum++;
 		BufferedImage image = new BufferedImage(Ressources.WINDOW.width, Ressources.WINDOW.height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = (Graphics2D) image.getGraphics();
-		for(int i=0;i*Ressources.RASTERSIZE<=Ressources.WINDOW.width;i++){
+		for(int i=0;i*Ressources.RASTERSIZE<Ressources.WINDOW.width;i++){
 			g.drawImage(getNextMapElement(),i*Ressources.RASTERSIZE,0,null);
 			if(DebugSettings.vg1tilenum)
 				renderText(g,((float) (Ressources.STORYTEXTSIZE/Ressources.SCALE)), lastTileType+"", (i*Ressources.RASTERSIZE)+20,100);
