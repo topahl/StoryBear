562e7b5c76f97ebbf5a73b488e5fc7b8ec82c7c2
Martika <martikaschwan@yahoo.de>
2014-04-25 21:41:57 +0200

    Bugfixing Interactionrenderer
    task #66
    
    Change-Id: I01f5c715e103a9a9ee5a4c3421e50c5caba8a0cd
    Signed-off-by: Martika <martikaschwan@yahoo.de>

--- StoryBear/src/com/opticalcobra/storybear/game/InteractionRenderer.java
+++ StoryBear/src/com/opticalcobra/storybear/game/InteractionRenderer.java
@@ -24,7 +24,8 @@ public class InteractionRenderer extends Renderer{
 
 	private int panelnum = 0;
 	private ArrayList<ILevelAppearance> storyInfoElements;
-	private Ringbuffer<TileResult> ringbuffer; 
+	private Ringbuffer<TileResult> ringbuffer;
+	int elementPointer = 0;
 	
 	
 	public InteractionRenderer(Ringbuffer<TileResult> ringbuffer, ArrayList<ILevelAppearance> elememts){		
@@ -35,37 +36,30 @@ public class InteractionRenderer extends Renderer{
 	
 	public void getNextViewPart(JLabel pane) {
 		
-		int elementPointer = 0;
-		panelnum++;
+		
 		BufferedImage image = new BufferedImage(Ressources.WINDOW.width, Ressources.WINDOW.height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = (Graphics2D) image.getGraphics();
 		
-//		for (int j = 2; j < 50; j++){
-//			System.out.println("" + ringbuffer.top(j).getTileType());
-//		}
-		
-		
-		
 		for (int i = 0; i < Ressources.TILESPERPANEL; i++){
 			//render collectable and character
-			if (storyInfoElements.get((panelnum - 1 + elementPointer)%Ressources.TILESPERPANEL) instanceof Collectable ||
-					storyInfoElements.get((panelnum - 1 + elementPointer)%Ressources.TILESPERPANEL) instanceof Character){
+			if (storyInfoElements.get(elementPointer) instanceof Collectable ||
+					storyInfoElements.get(elementPointer) instanceof Character){
 				
-				if (panelnum <= 3){
-					storyInfoElements.get((panelnum - 1 + elementPointer)%Ressources.TILESPERPANEL).render(g, ringbuffer.top(panelnum + 1 + i).getTileType(), Ressources.LAYERINTERACTION, pane);
-					System.out.println("Collectbale auf ID " + ringbuffer.top(panelnum + 1 + i).getTileType());
+				if (panelnum < 3){
+					storyInfoElements.get(elementPointer).render(g, ringbuffer.top((panelnum)*Ressources.TILESPERPANEL + i +2).getTileType(), Ressources.LAYERINTERACTION, pane);
 				} else{
-					storyInfoElements.get((panelnum - 1 + elementPointer)%Ressources.TILESPERPANEL).render(g, ringbuffer.top(2*Ressources.TILESPERPANEL + 1).getTileType(), Ressources.LAYERINTERACTION, pane);
+					storyInfoElements.get(elementPointer).render(g, ringbuffer.top(2*Ressources.TILESPERPANEL + 1 +i).getTileType(), Ressources.LAYERINTERACTION, pane);
 				}
 			}
 			
 			//Wenn mehrere Elemente auf eine Kachel gerendert werden, darf i nicht hochgezählt werden
-			if (storyInfoElements.get((panelnum -1 + elementPointer)%Ressources.TILESPERPANEL).getBlock() == 
-					storyInfoElements.get((panelnum + elementPointer)%Ressources.TILESPERPANEL).getBlock()){
+			if (storyInfoElements.get(elementPointer).getBlock()%Ressources.TILESPERPANEL == 
+					storyInfoElements.get(elementPointer + 1).getBlock()%Ressources.TILESPERPANEL){
 				i--;
 			}
 			elementPointer++;
 		}
+		panelnum++;
 		pane.setIcon(new ImageIcon(image));
 	}
 }
