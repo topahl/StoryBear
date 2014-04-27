5bf52a20078573da7da426029ac866af4fed894e
Miriam <Miriam@MI-I15z>
2014-04-27 12:31:32 +0200

    Geschichten werden bei Levelauswahl für Highscore angezeigt
    Change-Id: I732449c0b8148f76f171e639af406a55bbd309e4
    Signed-off-by: Miriam <Miriam@MI-I15z>

--- StoryBear/src/com/opticalcobra/storybear/menu/HighscoreList.java
+++ StoryBear/src/com/opticalcobra/storybear/menu/HighscoreList.java
@@ -1,14 +1,45 @@
 package com.opticalcobra.storybear.menu;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListDataListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+import com.opticalcobra.storybear.db.Database;
+import com.opticalcobra.storybear.editor.Story;
+import com.opticalcobra.storybear.editor.StoryInfo;
+import com.opticalcobra.storybear.main.User;
+//import com.opticalcobra.storybear.menu.UserPanel.UserList;
+//import com.opticalcobra.storybear.menu.UserPanel.UserListCellRenderer;
 import com.opticalcobra.storybear.res.Ressources;
 
 public class HighscoreList extends JLayeredPane {
+	
+	private ArrayList<StoryInfo> list;
+	private JList level;
+	private TextButton selectLevel;
+	private Database db = new Database();
+	private DefaultListModel<StoryInfo> model = new DefaultListModel<StoryInfo>();
 	
 	public HighscoreList(){
 
@@ -25,7 +56,9 @@ public class HighscoreList extends JLayeredPane {
 		headline.setText("Bestenliste");
 		add(headline, javax.swing.JLayeredPane.DEFAULT_LAYER);
 		
-		JTextArea text = new JTextArea();
+		
+		
+		/*JTextArea text = new JTextArea();
         text.setLocation((int)(40/Ressources.SCALE), (int)(125/Ressources.SCALE));
         text.setSize((int)(600/Ressources.SCALE), (int)(800/Ressources.SCALE));
         text.setLineWrap(true);
@@ -36,7 +69,7 @@ public class HighscoreList extends JLayeredPane {
         text.setOpaque(false);
         text.setFont(Menu.fontHeadline[2]);
         text.setForeground(Color.black);
-        add(text, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        add(text, javax.swing.JLayeredPane.DEFAULT_LAYER);*/
         
         
         //left Book Page
@@ -51,7 +84,48 @@ public class HighscoreList extends JLayeredPane {
 		headline.setText("Levelauswahl");
 		add(headline, javax.swing.JLayeredPane.DEFAULT_LAYER);
 		
-		text = new JTextArea();
+		
+		this.level = new JList<StoryInfo>();
+		this.level.setCellRenderer(new LevelListCellRenderer());
+		this.level.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.level.setOpaque(false);
+		this.level.setBackground(new Color(0,0,0,0));
+		this.level.setFont(Menu.fontText[0]);
+		this.level.setForeground(Color.black);
+		this.level.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				selectLevel.setEnabled(true);
+			}
+        });
+		this.loadStories();
+        
+		JScrollPane scrollpane = new Scrollbar(Ressources.SHELFCOLOR);
+		scrollpane.setViewportView(this.level);
+		scrollpane.getViewport().setOpaque(false);
+		scrollpane.getViewport().setBackground(new Color(0,0,0,0));
+		scrollpane.setOpaque(false);
+		scrollpane.setBackground(new Color(0,0,0,0));
+		scrollpane.setBorder(null);
+		JScrollBar sb = scrollpane.getVerticalScrollBar();
+		sb.setPreferredSize(new Dimension(30,0));
+        sb.setBackground(Ressources.SHELFCOLOR);
+        scrollpane.setBounds((int)(31/Ressources.SCALE), (int)(125/Ressources.SCALE), (int)(600/Ressources.SCALE), (int)(315/Ressources.SCALE));
+        add(scrollpane, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        
+        this.selectLevel = new TextButton("Level wählen", 45, 150, 250, 50);
+        this.selectLevel.setSize(315, 50);
+        this.selectLevel.setLocation(31, 470);
+        this.selectLevel.setEnabled(false);
+        /*this.selectLevel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				selectLevel();
+			}
+		});*/
+        add(this.selectLevel, javax.swing.JLayeredPane.DEFAULT_LAYER);
+		
+		/*text = new JTextArea();
         text.setLocation((int)(40/Ressources.SCALE), (int)(125/Ressources.SCALE));
         text.setSize((int)(600/Ressources.SCALE), (int)(800/Ressources.SCALE));
         text.setLineWrap(true);
@@ -62,9 +136,56 @@ public class HighscoreList extends JLayeredPane {
         text.setOpaque(false);
         text.setFont(Menu.fontHeadline[2]);
         text.setForeground(Color.black);
-        add(text, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        add(text, javax.swing.JLayeredPane.DEFAULT_LAYER);*/
 		
 		setBounds(0, 0, Menu.innerPanel.width, Menu.innerPanel.height);
 	}
+	
+	
+	private void loadStories() {
+		ArrayList<StoryInfo> story = db.getAllStorysFromDatabase();
+		model.clear();
+		for(int i=0 ; i<story.size();i++){
+			model.addElement(story.get(i));
+		}
+        
+        this.level.setModel(model);		
+	}
+	
+
+	
+	
+	private class LevelListCellRenderer extends DefaultListCellRenderer {
+		private JLabel title;
+		private JLabel user;
+	     @Override
+	     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+	         //JComponent c = (JComponent) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+	         JPanel c = new JPanel();
+	    	 c.setBackground(new Color(0,0,0,0));
+	         c.setBorder(null);
+	         c.setCursor(Ressources.CURSORCLICKABLE);
+	         if (isSelected) {
+	             c.setForeground(Ressources.MENUCOLORSELECTED);
+	         }
+	         
+	         Story story = ((StoryInfo) value).getStory();
+	         user = new JLabel();
+	         user.setForeground(new Color(92, 90, 90));
+	 		 title = new JLabel();
+	 		 title.setText(story.getTitle());
+	 		 user.setText("   von "+story.getAuthor().getName());
+	 		 c.add(title);
+	 		 c.add(user);
+	 		 
+	 		 if(isSelected){
+	 			title.setForeground(new Color(186, 15, 15));
+				user.setForeground(new Color(186, 15, 15));
+	 		 }
+	         
+	         return c;
+	     }
+	}
+	
 
 }
